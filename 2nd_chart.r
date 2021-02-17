d8210a2ff66bd00caec85f3314204f7ef04fcc8a
abdisharif <77075009+abdisharif@users.noreply.github.com>
2021-02-17 12:56:38 -0800

    Update 2nd_chart.r

--- 2nd_chart.r
+++ 2nd_chart.r
@@ -1,4 +1,9 @@
-## Load Data
-Data <- read.csv("")
+# Plot of Black Population Incarceration Trend ----------------------------
+p1 <- ggplot() + 
+  geom_line(aes(y = X2013, x =  Black.or.African.American.1),
+            data = Arrest_Information) 
+scale_x_continuous(breaks=seq(2013,2019,2))
+theme(text=element_text(family="Tahoma"))
 
-# second chart Data
+
+p1 + labs(title = "Mass Incarceration", x = "Black Population in Jail", y = "Years", caption = "By: Abdirahim M. ")
