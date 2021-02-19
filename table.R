35a51abf6edfb2f5937bb52ffb2d4d3953d04ee1
mschrier <matthewrobertschrier@gmail.com>
2021-02-19 01:18:57 -0800

    more changes

--- table.R
+++ table.R
@@ -6,40 +6,14 @@ library(dplyr)
 Data <- read.csv("/Users/matthewschrier/Desktop/INFO_201/https-classroom.github.com-a-XrLzm1Hu/Arrest_Information.csv")
 View(Data)
 
+Data1 <- group_by(Data, X2013)
 
-
-
-# Data division by year
-Data_2013_all <- filter(Data, X2013=="2013")
-Data_2015_all <- filter(Data, X2013=="2015")
-Data_2017_all <- filter(Data, X2013=="2017")
-Data_2019_all <- filter(Data, X2013=="2019")
-
-# race_with_the_highest_crime_rate_in_2019
-race_with_the_highest_crime_rate_in_2019 <- select(Data_2019_all, White,Black.or.African.American,American.Indian.or.Alaska.Native,Asian,Native.Hawaiian.or.Other.Pacific.Islander)
-race_with_the_highest_crime_rate_in_2019 <- colnames(race_with_the_highest_crime_rate_in_2019)[max.col(race_with_the_highest_crime_rate_in_2019,ties.method="first")]
-race_with_the_highest_crime_rate_in_2019 <- race_with_the_highest_crime_rate_in_2019[1]
-
-# highest crime type for African Americans in 2013
-Data_2013 <- Data_2013_all[-c(1,29),]
-highest_crime_type_for_African_Americans_in_2013 <- as.vector(Data_2013[which.max(Data_2013$Black.or.African.American),1])
-highest_crime_type_for_African_Americans_in_2013
-
-#highest crime type for African Americans in 2019
-Data_2019 <- Data_2019_all[-c(1,29),]
-highest_crime_type_for_African_Americans_in_2019 <- as.vector(Data_2019[which.max(Data_2019$Black.or.African.American),1])
-highest_crime_type_for_African_Americans_in_2019 
-# average arrests total for 2013, 2015,2017, and 2019
-average_criminal_total <- mean(Data_2013_all$Total[1],Data_2015_all$Total[1],Data_2017_all$Total[1],Data_2019_all$Total[1])
-average_criminal_total
-
-# highest crime type for white in 2013
-highest_crime_type_for_white_in_2013 <- as.vector(Data_2013[which.max(Data_2013$White),1])
-highest_crime_type_for_white_in_2013 
-
-# highest crime type for white in 2019
-highest_crime_type_for_white_in_2019 <- as.vector(Data_2019[which.max(Data_2019$White),1])
-highest_crime_type_for_white_in_2019
-
-Table<- data.frame(race_with_the_highest_crime_rate_in_2019,highest_crime_type_for_African_Americans_in_2013,highest_crime_type_for_African_Americans_in_2019,average_criminal_total,highest_crime_type_for_white_in_2013,highest_crime_type_for_white_in_2019 )
+Table <- summarise(Data1, total_arrest = max(Total), 
+                   black_arrest = max(Black.or.African.American), white_arrests = max(White),
+                   American_Indian_or_Alaska_Native_arrest = max(American.Indian.or.Alaska.Native),
+                   asian_arrests = max(Asian), Native_Hawaiian_or_Other_Pacific_Islander_arrest = max(Native.Hawaiian.or.Other.Pacific.Islander))
 View(Table)
+
+
+
+
