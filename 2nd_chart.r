16729bac1076702313c4f2ee17cfac50c9c25b6d
abdisharif <77075009+abdisharif@users.noreply.github.com>
2021-02-18 17:04:10 -0800

    Update 2nd_chart.r

--- 2nd_chart.r
+++ 2nd_chart.r
@@ -1,9 +1,8 @@
-## Install Package
 install.packages("ggplot2")
 library(ggplot2)
 library(dplyr)
 ## Load Data
-Data <- read.csv("/Users/matthewschrier/Desktop/INFO_201/https-classroom.github.com-a-XrLzm1Hu/Arrest_Information.csv")
+Data <- read.csv("https://raw.githubusercontent.com/mschrier/https-classroom.github.com-a-XrLzm1Hu/main/Arrest_Information.csv")
 
 # Data division by year
 Data_2013_all <- filter(Data, X2013=="2013")
@@ -16,4 +15,3 @@ Plot_2019 <- Data_2019_all[-c(1),c(1,2)]
 ggplot(Plot_2019, aes(x=Crime...Total.Arrest, y=Total)) + geom_bar(stat="identity")+
   ggtitle("Arrests in 2019")+ theme(plot.title = element_text(hjust = 0.5))+ theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) +
   xlab("Crimes") + ylab("Total Arrest")
-
