2195c9e1a0d3aae413f05d3481514b0136544527
mschrier <77217462+mschrier@users.noreply.github.com>
2021-02-28 18:47:25 -0800

    Update table.R

--- table.R
+++ table.R
@@ -1,20 +1,3 @@
-## Install Package
-install.packages("dplyr")
-library(dplyr)
-lint("table.R")
-
-## Load Data
-Data <- read.csv("https://raw.githubusercontent.com/mschrier/https-classroom.github.com-a-XrLzm1Hu/main/Arrest_Information.csv")
-View(Data)
-
-Data1 <- group_by(Data, X2013)
-
-Table <- summarise(Data1, total_arrest = max(Total), 
-                   black_arrest = max(Black.or.African.American), white_arrests = max(White),
-                   American_Indian_or_Alaska_Native_arrest = max(American.Indian.or.Alaska.Native),
-                   asian_arrests = max(Asian), Native_Hawaiian_or_Other_Pacific_Islander_arrest = max(Native.Hawaiian.or.Other.Pacific.Islander))
-                   
-View(Table)
 
 
 
