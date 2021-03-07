87d5039278e60e3270297ca921a2d0cee670358e
Confidence Orji <Confi564@gmail.com>
2021-03-06 18:29:18 -0800

    Update Introductory Page FD.R
    Intro done; image loading

--- Introductory Page FD.R
+++ Introductory Page FD.R
@@ -7,7 +7,9 @@
 <meta name="author" content="Confidence Orji, Aminata Diallo, Abdirahim Mohamed, Matthew Schrier" />
 
 <title>Mass Incarceration</title>
-
+<h2>Introduction:</h2>
+<p><em>Mass incarceration has boggled the mind of my teammates and I for some time; so we set out to learn more. In this great 'land of the free' 'home of the brave', why is there such an embarassingly high number of incarceration? And why does it seem to target a specific group? Black Americans. What crimes are they most indicted for compared to other races? Is the rate of incarceration going up or is it finally starting to come down? Join us as we learn more and try to unravel this mystery!</em></p>
+<img src=" />
 </style>
 </head>
 </html>
